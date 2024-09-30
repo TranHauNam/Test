70bbc9f4153b384f246b7445e11bd6346e82c415
TranHauNam <haunam3112@gmail.com>
2024-09-30 20:23:26 +0700

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,73 @@
+function calculateDiscount(isLoyalCustomer, productQuantity, isPromotionPeriod) {
+    if (productQuantity < 0 ) {
+        return "Số sản phẩm không hợp lệ";
+    }
+    var discount = 0; 
+    if (isLoyalCustomer) {
+        if (productQuantity > 10) {
+            if (isPromotionPeriod) {
+                discount = 30;
+            } else {
+                discount = 20;
+            }
+        } else {
+            if (isPromotionPeriod) {
+                discount = 10;
+            } else {
+                discount = 5;
+            }
+        }
+    } else {
+        if (productQuantity > 10) {
+            if (isPromotionPeriod) {
+                discount = 20;
+            } else {
+                discount = 10;
+            }
+        } else {
+            if (isPromotionPeriod) {
+                discount = 5;
+            } else {
+                discount = 0;
+            }
+        }
+    }
+    return discount;
+}
+
+// Kiểm thử bảng quyết định
+console.log(calculateDiscount(true, -1, true));
+console.log(calculateDiscount(true, -1, false));
+console.log(calculateDiscount(true, 1, true));
+console.log(calculateDiscount(true, 1, false));
+console.log(calculateDiscount(true, 11, true));
+console.log(calculateDiscount(true, 11, false));
+console.log(calculateDiscount(false, -1, true));
+console.log(calculateDiscount(false, -1, false));
+console.log(calculateDiscount(false, 1, true));
+console.log(calculateDiscount(false, 1, false));
+console.log(calculateDiscount(false, 11, true));
+console.log(calculateDiscount(false, 11, false));
+
+
+// Kiểm thử biên
+console.log(calculateDiscount(true, 0, true));
+console.log(calculateDiscount(true, 1, true));
+console.log(calculateDiscount(true, 1000, true));
+console.log(calculateDiscount(true, 99999, true));
+console.log(calculateDiscount(true, 100000, true));
+console.log(calculateDiscount(true, 0, false));
+console.log(calculateDiscount(true, 1, false));
+console.log(calculateDiscount(true, 1000, false));
+console.log(calculateDiscount(true, 99999, false));
+console.log(calculateDiscount(true, 100000, false));
+console.log(calculateDiscount(false, 0, true));
+console.log(calculateDiscount(false, 1, true));
+console.log(calculateDiscount(false, 1000, true));
+console.log(calculateDiscount(false, 99999, true));
+console.log(calculateDiscount(false, 100000, true));
+console.log(calculateDiscount(false, 0, false));
+console.log(calculateDiscount(false, 1, false));
+console.log(calculateDiscount(false, 1000, false));
+console.log(calculateDiscount(false, 99999, false));
+console.log(calculateDiscount(false, 100000, false));
