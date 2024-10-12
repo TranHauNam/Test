05938fe11b0999c410a2ee4af542d44d018660fb
TranHauNam <haunam3112@gmail.com>
2024-10-12 14:29:58 +0700

    Test C2

--- javascript.js
+++ javascript.js
@@ -36,6 +36,7 @@ function calculateDiscount(isLoyalCustomer, productQuantity, isPromotionPeriod) 
 }
 
 // Kiểm thử bảng quyết định
+console.log("Kiểm thử bảng quyết định:");
 console.log(calculateDiscount(true, -1, true));
 console.log(calculateDiscount(true, -1, false));
 console.log(calculateDiscount(true, 1, true));
@@ -51,6 +52,7 @@ console.log(calculateDiscount(false, 11, false));
 
 
 // Kiểm thử biên
+console.log("Kiểm thử biên:");
 console.log(calculateDiscount(true, 0, true));
 console.log(calculateDiscount(true, 1, true));
 console.log(calculateDiscount(true, 1000, true));
@@ -71,3 +73,15 @@ console.log(calculateDiscount(false, 1, false));
 console.log(calculateDiscount(false, 1000, false));
 console.log(calculateDiscount(false, 99999, false));
 console.log(calculateDiscount(false, 100000, false));
+
+//Kiểm thử theo độ phủ cấp 2(C2)
+console.log("Kiểm thử theo độ phủ cấp 2(C2):");
+console.log(calculateDiscount(true, -1, true));
+console.log(calculateDiscount(true, 11, true));
+console.log(calculateDiscount(true, 11, false));
+console.log(calculateDiscount(true, 1, true));
+console.log(calculateDiscount(true, 1, false));
+console.log(calculateDiscount(false, 11, true));
+console.log(calculateDiscount(false, 11, false));
+console.log(calculateDiscount(false, 1, true));
+console.log(calculateDiscount(false, 1, false));
